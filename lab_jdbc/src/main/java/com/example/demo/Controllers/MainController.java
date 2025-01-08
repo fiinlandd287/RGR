e11e6e7c00ef57d558571dba3c885f67e0438b05
fiinlandd287 <fiinlandd@gmail.com>
2025-01-08 19:26:13 +0200

    Add files via upload

---
+++ lab_jdbc/src/main/java/com/example/demo/Controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo.Controllers;
+
+//import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+}
